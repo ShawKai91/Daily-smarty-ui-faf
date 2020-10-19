8cfb56ec1316b098daf386533ef25ab4360127a6
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-10-19 15:43:05 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,10 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -13,9 +14,15 @@ export default function(state = INIT_STATE, action) {
 
             const recentPosts = action.payload;
             return {
-                    ...state,
-                    recentPosts
+                ...state,
+                recentPosts
                 }
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state, 
+                resultsPosts
+            }
         default:
             return state;
     }
